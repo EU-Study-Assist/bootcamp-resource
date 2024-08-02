d8cc7c1663d32e65ebe9d8af299670af7ccaa08a
xrander <olamideadu@gmail.com>
2024-08-02 20:32:38 +0200

    Recap - workflow - packages
    Importing Data
    - csv
    - txt files

--- introductory_class_2.r
+++ introductory_class_2.r
@@ -1,6 +1,48 @@
 
 # Importing Data ----------------------------------------------------------
 
+# File Formats ------------------------------------------------------------
+## Excel files
+".xls"
+".xlsx"
+## Word
+".docx"
 
+# R
+".r"
+
+## SPSS
+".sav"
+
+## Stata
+".do"
+".dta"
+
+## LibreCalc
+".ods"
+
+# Importing Text and CSV Files --------------------------------------------
+library("tidyverse")
+
+read.csv("data/redpine.csv")
+read_csv("data/redpine.csv")
+
+## Text file -----------
+read_delim(file = "data/dbhlist_exp1012.txt", delim = "\t")
+read_delim(file = "data/plantgrowth.csv", delim = ",")
+
+## CSV Files
+read_csv("data/plantgrowth.csv")
+
+# Excel Files -------------------------------------------------------------
+
+
+# Google Sheet Files ------------------------------------------------------
+
+
+# LibreOffice Files -------------------------------------------------------
+
+
+# Exporting Files ---------------------------------------------------------
 
 
